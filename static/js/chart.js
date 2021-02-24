082d038de9a83cfb703167ea30e4f95082c55bc1
Imran Hossen <imranbappy.official@gmail.com>
2021-02-24 20:10:17 +0600

    done

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,24 +1,59 @@
-var addToCat = document.querySelectorAll("#cartadd");
-var idcatcher = document.getElementById('contaty');
-var allprice = 0;
-var foodname = []
-var contaty = 0;
-addToCat.forEach((idpicer) => {
-    idpicer.addEventListener('click', () => {
-        foodname.push(idpicer.dataset.name);
-        allprice += Number(idpicer.dataset.price);
-        contaty += 1;
-        const quantity = setItems(contaty, foodname, allprice);
-        idcatcher.innerHTML = quantity;
-        console.log(quantity);
-    });
+var addBtn = document.querySelectorAll("#cart");
+addBtn.forEach(btn => {
+    let id = btn.dataset.id, name = btn.dataset.name, price = btn.dataset.price, quantity = 1
+    btn.addEventListener("click", (e) => {
+        addItem({ id, name, price, quantity });
+    })
 })
 
-function setItems(item1, item2, item3) {
-    const allItems = JSON.stringify({ quantity: item1, name: item2, price: item3 });
-    localStorage.setItem('cart', allItems);
-    let items = localStorage.getItem('cart')
-    return JSON.parse(items).quantity
+function addItem(item) {
+    let getFoods = getItem()
+
+    if (Object.keys(getFoods).length === 0) {
+        localStorage.setItem('userId', JSON.stringify({ items: [item] }));
+    } else {
+        let result = getFoods.items.find(i => i.id === item.id)
+        if (result) {
+            let allFood = []
+            getFoods.items.map(i => {
+                if (i.id === item.id) {
+                    i.quantity = updateQuantity(getFoods.items, item)[1]
+                    allFood.push(i)
+                } else {
+                    allFood.push(i)
+                }
+            })
+            localStorage.setItem('userId', JSON.stringify({ items: allFood }));
+        } else {
+            localStorage.setItem('userId', JSON.stringify({ items: [...getFoods.items, item] }));
+        }
+    }
+
+    updateUi(getFoods.items)
 }
 
-idcatcher.innerText = JSON.parse(localStorage.getItem('cart')).quantity;
+
+function getItem() {
+    return JSON.parse(localStorage.getItem('userId')) || {};
+}
+function updateQuantity(items, newItem) {
+    let result = items.find(i => i.id === newItem.id)
+    if (result) {
+        let quantity = result["quantity"]
+        quantity += 1
+        return [result, quantity]
+    }
+}
+
+
+function updateUi(items) {
+    let quantity = 0, price = 0;
+    items.map(i => {
+        quantity += Number(i.quantity)
+        price += Number(i.price) * Number(i.quantity)
+    })
+    document.getElementById('item').innerText = quantity
+    document.getElementById('price').innerText = price
+}
+
+updateUi(getItem().items)
